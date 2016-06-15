e222b47c3b81420854025585b7a84f29a37b71a4
victorCB96 <victor_cb_96@hotmail.com>
2016-06-15 11:45:41 -0600

    se arregla el metodo que estaba mal

--- app/src/main/java/com/example/diego/proyectoseguridad/Modelo/clsManejoPeliculas.java
+++ app/src/main/java/com/example/diego/proyectoseguridad/Modelo/clsManejoPeliculas.java
@@ -2,11 +2,13 @@ package com.example.diego.proyectoseguridad.Modelo;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 /**
  * Created by victor on 15/06/2016.
  */
 public class clsManejoPeliculas {
+    private SQLiteDatabase bd;
     private clsConexion conexion;
     private static final String TABLA_PELICULAS="tbPeliculas";
     private static final String TABLA_GENEROS="tbGeneros";
@@ -17,10 +19,10 @@ public class clsManejoPeliculas {
         this.conexion=new clsConexion(context);
     }
 
-    public Cursor getGenerosPeliculas(Pelicula pelicula)
+    public Cursor getGenerosPeliculas(int idPelicula)
     {
-        String query;
-        query="select g.genero gp.idPelicula from tbGeneros g inner join tbGeneroPelicula gp on(g.idGenero=gp.idGenero) where gp.idPelicula='"+pelicula.getIdPelicula()+"'";
-        return conexion.mConsultar(query);
+        Cursor cursor;
+        cursor=bd.rawQuery("select g.genero gp.idPelicula from tbGeneros g inner join tbGeneroPelicula gp on(g.idGenero=gp.idGenero) where gp.idPelicula=?",new String[]{String.valueOf(idPelicula)});
+        return cursor;
     }
 }
