2f0b76d6e235d887e93c609b1c3ba51e9e05b5fb
victorCB96 <victor_cb_96@hotmail.com>
2016-06-27 19:24:32 -0600

    cambio en la base

---
+++ app/src/main/java/com/example/diego/proyectoseguridad/Modelo/clsManejoBitacoras.java
@@ -0,0 +1,28 @@
+package com.example.diego.proyectoseguridad.Modelo;
+
+import android.content.ContentValues;
+import android.content.Context;
+
+/**
+ * Created by victor on 26/06/2016.
+ */
+public class clsManejoBitacoras {
+
+    private clsConexion conexion;
+    public static final String TABLA_BITACORAS="tbBitacoras";
+
+    public clsManejoBitacoras(Context context) {
+        this.conexion = new clsConexion(context);
+    }
+
+    public boolean mAgregarBitacora(Bitacora bitacora) {
+        ContentValues valores = new ContentValues();
+        valores.put("accion", bitacora.getAccion());
+        valores.put("descripcion", bitacora.getDescripcion());
+        valores.put("hora",(bitacora.getHora()).toString());
+        valores.put("idUsuario", bitacora.getIdUsuario());
+
+        return conexion.mInsertar(valores, TABLA_BITACORAS);
+    }
+
+}
