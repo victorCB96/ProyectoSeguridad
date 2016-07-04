63981d43e81862e8d5fb9b6534eb2c81b2566d10
Diego Castro <diego.castro.moya@gmail.com>
2016-07-04 10:57:36 -0600

    sdsdss

--- app/src/main/java/com/example/diego/proyectoseguridad/Vista/LoginActivity.java
+++ app/src/main/java/com/example/diego/proyectoseguridad/Vista/LoginActivity.java
@@ -9,15 +9,23 @@ import android.view.View.OnClickListener;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.diego.proyectoseguridad.Modelo.Bitacora;
 import com.example.diego.proyectoseguridad.Modelo.Usuario;
+import com.example.diego.proyectoseguridad.Modelo.clsManejoBitacoras;
 import com.example.diego.proyectoseguridad.Modelo.clsManejoUsuarios;
 import com.example.diego.proyectoseguridad.R;
 import com.mobsandgeeks.saripaar.ValidationError;
 import com.mobsandgeeks.saripaar.Validator;
 import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 /**
  * A login screen that offers login via email/password.
@@ -35,15 +43,21 @@ public class LoginActivity extends AppCompatActivity implements Validator.Valida
 
     private Button login;
     private clsManejoUsuarios conexion;
+    private clsManejoBitacoras clsBitacoras;
     private Validator validator;
     private View view;
     private Usuario usuario;
+    private Bitacora bitacora;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         conexion = new clsManejoUsuarios(this);
+        clsBitacoras = new clsManejoBitacoras(this);
+        bitacora= new Bitacora();
+        Toast.makeText(this,"hora------"+getDateCurrentTimeZone(),
+                Toast.LENGTH_SHORT).show();
 
         //Inicializacion de los elementos del xml
         email=(AutoCompleteTextView)findViewById(R.id.email);
@@ -70,11 +84,42 @@ public class LoginActivity extends AppCompatActivity implements Validator.Valida
         finish();
     }
 
+    public String obtenerFecha(){
+        final Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(System.currentTimeMillis());
+        Date date = cal.getTime();
+        long mHour = date.getTime();
+        int mMinute = date.getMinutes();
+        String fecha;
+
+        return fecha=""+mHour;
+    }
+    public  String getDateCurrentTimeZone() {
+        try{
+            Calendar calendar = Calendar.getInstance();
+            TimeZone tz = TimeZone.getDefault();
+            calendar.getTimeInMillis();
+            calendar.add(Calendar.MILLISECOND, tz.getOffset(calendar.getTimeInMillis()));
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+            Date currenTimeZone = (Date) calendar.getTime();
+            return sdf.format(currenTimeZone);
+        }catch (Exception e) {
+        }
+        return "";
+    }
+
     @Override
     public void onValidationSucceeded() {
         usuario = conexion.consultarUsuario(password.getText().toString().trim(), email.getText().toString().trim());
-
+        bitacora.setIdUsuario(usuario.getIdUsuario());
+        bitacora.setAccion("Logueo");
+        bitacora.setDescripcion("Se ha conectado a la app");
+        bitacora.setHora(Time.valueOf(getDateCurrentTimeZone()));
         if(usuario != null){
+            if(clsBitacoras.mAgregarBitacora(bitacora)){
+                Toast.makeText(this,"Conectado correctamente",
+                        Toast.LENGTH_SHORT).show();
+            }
             Intent mainIntent = new Intent(this, MainActivity.class);
             mainIntent.putExtra(USUARIO, usuario );
             startActivity(mainIntent);
