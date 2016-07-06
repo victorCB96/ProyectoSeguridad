52efea19a545904d9eb3e1a290800124b17f7109
victorCB96 <victor_cb_96@hotmail.com>
2016-07-06 12:27:52 -0600

    encriptacion

---
+++ app/src/main/java/com/example/diego/proyectoseguridad/Modelo/EncriptarContra.java
@@ -0,0 +1,48 @@
+
+package com.example.diego.proyectoseguridad.Modelo;
+
+import android.util.Base64;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+//import sun.misc.BASE64Decoder;
+//import sun.misc.BASE64Encoder;
+
+/**
+ * Created by victor on 06/07/2016.
+ */
+public class EncriptarContra {
+    public static final String md5(final String s) {
+        final String MD5 = "MD5";
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest
+                    .getInstance(MD5);
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuilder hexString = new StringBuilder();
+            for (byte aMessageDigest : messageDigest) {
+                String h = Integer.toHexString(0xFF & aMessageDigest);
+                while (h.length() < 2)
+                    h = "0" + h;
+                hexString.append(h);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
